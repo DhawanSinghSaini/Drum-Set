a7533774939ad1d4fdf37bba52401367457119c8
DhawanSinghSaini <161929806+DhawanSinghSaini@users.noreply.github.com>
2024-09-13 13:28:03 +0530

    index.js

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ for(i=0;i<document.querySelectorAll(".drum").length;i++){
     document.addEventListener("keydown", buttonAnimation);
 }
     
-var w = new Audio("./sounds/tom-1.mp3")
-var a = new Audio("./sounds/tom-2.mp3")
-var s = new Audio("./sounds/tom-3.mp3")
-var d = new Audio("./sounds/tom-4.mp3")
-var j = new Audio("./sounds/crash.mp3")
-var k = new Audio("./sounds/kick-bass.mp3")
-var l = new Audio("./sounds/snare.mp3")
+var w = new Audio("./tom-1.mp3")
+var a = new Audio("./tom-2.mp3")
+var s = new Audio("./tom-3.mp3")
+var d = new Audio("./tom-4.mp3")
+var j = new Audio("./crash.mp3")
+var k = new Audio("./kick-bass.mp3")
+var l = new Audio("./snare.mp3")
     
 function handelclick(){
     var sound = this.innerHTML;
